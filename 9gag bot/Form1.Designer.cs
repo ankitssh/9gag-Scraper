0639e9bcac4d50996ab2631a57bf570d0d82158b
soulxhacker <godotcoffee231@gmail.com>
2017-04-22 11:01:17 +0530

    Initial Commit

---
+++ 9gag bot/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace _9gag_bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.total = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Enter a 9gag image url here";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(229, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of images to download";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 144);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(272, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.BackColor = System.Drawing.Color.Transparent;
+            this.total.Location = new System.Drawing.Point(113, 172);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(46, 17);
+            this.total.TabIndex = 5;
+            this.total.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 198);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "9Gag Scraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label total;
+    }
+}
+
---
+++ 9gag bot/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+namespace _9gag_bot
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker wb = new BackgroundWorker();
+                       
+        string sourceCode;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum =int.Parse( textBox2.Text);
+            total.Text="0/"+textBox2.Text;
+            textBox2.TextChanged += textBox2_TextChanged;
+           // wb.DoWork += wb_DoWork;
+        }
+
+        void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            total.Text = "0/" + textBox2.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+           
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+
+                for (int i = 0; i <int.Parse( textBox2.Text); i++)
+                {
+
+
+                    string url = textBox1.Text;
+
+
+
+                    WebClient wc = new WebClient();
+                    try
+                    {
+                        Parallel.Invoke(() =>   sourceCode = wc.DownloadString(url));
+                      
+                    }
+                    catch (Exception ex) { }
+
+                    int headerIndexStart = sourceCode.IndexOf("<h2 class=\"badge-item-title\">")+29;
+                    int headerEndIndex = sourceCode.IndexOf("<",headerIndexStart);
+                    string header = sourceCode.Substring(headerIndexStart,headerEndIndex-headerIndexStart);
+                    if (header.Contains("&#039;"))
+                    header = header.Replace("&#039;","'");
+                    
+
+                    int startIndex = sourceCode.IndexOf("<img class=\"badge-item-img\" src=\"") + 33;
+                    int endIndex = sourceCode.IndexOf("\"", startIndex);
+
+                    string picUrl = sourceCode.Substring(startIndex, endIndex - startIndex);
+                    string extension = picUrl.Substring(picUrl.Length - 4);
+                    picUrl = picUrl.Substring(0, picUrl.Length - 4);
+                    try
+                    {
+                        wc.DownloadFileAsync(new Uri(picUrl + extension), fb.SelectedPath+"//" + header + extension);
+                     
+                        
+                      
+                    }
+                        
+                    catch (Exception ex) { }
+                    finally
+                    {
+                        Parallel.Invoke(()=>   total.Text = i + "/" + textBox2.Text);
+                        total.Refresh();
+                     
+
+                        progressBar1.Value = progressBar1.Value + 1;   
+              
+                        startIndex = sourceCode.IndexOf("badge-fast-entry badge-next-post-entry next");
+                        startIndex = sourceCode.IndexOf("href=", startIndex) + 6;
+
+                        endIndex = sourceCode.IndexOf("\"", startIndex);
+
+                        string nextUrl = sourceCode.Substring(startIndex, endIndex - startIndex);
+                       textBox1.Text= "https://9gag.com" + nextUrl;
+                    }
+                }
+
+                MessageBox.Show("Download Completed!");
+            
+            }
+
+
+
+            progressBar1.Value = 0;
+            
+        }
+
+       
+     
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.SelectAll();
+        }
+    }
+}
